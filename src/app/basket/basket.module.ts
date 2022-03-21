ce152fc33a9d2464e4c239cdb0b7e9590b219112
Ratchapol <ratchapol.tum@gmail.com>
2022-03-21 12:41:20 +0700

    update module basket

--- src/app/basket/basket.module.ts
+++ src/app/basket/basket.module.ts
@@ -3,12 +3,11 @@ import { BasketService } from './basket.service';
 import { BasketController } from './basket.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Basket } from './entities/basket.entity';
-import { SequelizeMethod } from 'sequelize/types/utils';
 
 @Module({
   imports: [SequelizeModule.forFeature([Basket])],
   controllers: [BasketController],
   providers: [BasketService],
-  exports: [BasketService, SequelizeMethod],
+  exports: [BasketService, SequelizeModule],
 })
 export class BasketModule {}
--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -5,6 +5,8 @@ import { Account } from 'src/app/account/entities/account.entity';
 import { Active } from 'src/app/active/entities/active.entity';
 import { Address } from 'src/app/address/entities/address.entity';
 import { Basket } from 'src/app/basket/entities/basket.entity';
+import { ProductBrand } from 'src/app/product-brand/entities/product-brand.entity';
+import { Product } from 'src/app/product/entities/product.entity';
 import { RefreshToken } from 'src/app/refresh-token/entities/refresh-token.entity';
 
 export class DatabaseConfig {
@@ -19,7 +21,16 @@ export class DatabaseConfig {
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        models: [Active, AccountType, Account, RefreshToken, Address, Basket],
+        models: [
+          Active,
+          AccountType,
+          Account,
+          RefreshToken,
+          Address,
+          Basket,
+          ProductBrand,
+          Product,
+        ],
         autoLoadModels: true,
         synchronize: true,
       });
